df36aae863bddc4a48749d75f51aa9eb9efb86ba
IQ1981 <prinz1981a@gmail.com>
2026-05-11 00:45:28 +0200

    iitc-new-players-l1.user.js erstellen

---
+++ iitc-new-players-l1.user.js
@@ -0,0 +1,17 @@
+<h3>New Level 1 Players – IITC Plugin</h3>
+<p>Dieses Plugin erkennt automatisch neue Agenten (Level 1) anhand ihrer Level-1-Resonatoren auf Portalen und zeigt sie in einem übersichtlichen Popup an.</p>
+
+<h4>Funktionen</h4>
+<ul>
+  <li>Suchradius wählbar: 5 / 10 / 20 / 50 / 100 / 200 / 500 / 1000 km</li>
+  <li>Tabelle mit Agentname, Distanz, Erstsichtung und belegten Portalen</li>
+  <li>Karten-Marker (cyan) für alle erkannten L1-Agenten</li>
+  <li>◎ Button zum direkten Anspringen auf der Karte</li>
+  <li>Ingress-typisches Dark-UI mit Scanline-Effekt</li>
+</ul>
+
+<h4>Installation</h4>
+<p>Tampermonkey / Violentmonkey installieren, dann das Skript als neue UserScript-Datei einfügen.</p>
+
+<h4>Hinweis</h4>
+<p>Spieler werden nur erkannt, wenn du in deren Kartenbereich zoomst und die Portaldaten von der Intel-API geladen werden.</p>
